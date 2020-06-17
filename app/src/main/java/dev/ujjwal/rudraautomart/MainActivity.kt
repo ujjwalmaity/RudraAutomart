e21c009291b84935f39616dcc9a216e82bc30caa
ujjwal <ujjwalmaity98@gmail.com>
2020-06-17 22:43:42 +0530

    Sliding images using ViewFlipper

--- app/src/main/java/dev/ujjwal/rudraautomart/MainActivity.kt
+++ app/src/main/java/dev/ujjwal/rudraautomart/MainActivity.kt
@@ -2,10 +2,36 @@ package dev.ujjwal.rudraautomart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val images = arrayListOf(
+            R.drawable.a1,
+            R.drawable.a2,
+            R.drawable.a3
+        )
+
+        for (i in images) {
+            flipperImages(i)
+        }
+    }
+
+    private fun flipperImages(image: Int) {
+        val imageView = ImageView(this)
+        imageView.setBackgroundResource(image)
+        imageView.scaleType = ImageView.ScaleType.FIT_XY
+
+        view_flipper.addView(imageView)
+        view_flipper.flipInterval = 4000
+        view_flipper.isAutoStart = true
+
+        view_flipper.setInAnimation(this, R.anim.slide_in_right)
+        view_flipper.setOutAnimation(this, R.anim.slide_out_left)
     }
 }
